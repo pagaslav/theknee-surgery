5d1ff5cc0b7c92be78cb8ea52d3e8bd27b160be5
Artem Bryzh <aab@AMBM2.local>
2024-06-20 15:12:47 +0100

    Create structure for static files and templates

--- app.py
+++ app.py
@@ -61,10 +61,3 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-    
-
-# Original line
-mongo = PyMongo(app)
-
-# Modified line
-mongo = PyMongo(app, tlsCAFile=certifi.where())
