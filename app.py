6c65f31a7b715be45b3924514f1bd0b6564fe080
Artem Bryzh <pagaslav@gmail.com>
2024-07-09 12:11:14 +0100

    style: refactor app.py for 80-char line limit
    Refactore the code in app.py to ensure that each line is no longer than 80 characters. This improves
    readability and conforms to best practices for maintaining concise code.

--- app.py
+++ app.py
@@ -27,10 +27,12 @@ mongo = PyMongo(app, tlsCAFile=certifi.where())
 app.config["SESSION_PERMANENT"] = False  # Session is not permanent by default
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=30)
 
+
 @app.route("/")
 def index():
     # Render the main index page
     return render_template('index.html')
+
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -40,14 +42,16 @@ def signup():
         gender = request.form.get("gender")
         dob = request.form.get("dob")  # Get date of birth
         phone = request.form.get("phone")
-        email = request.form.get("email").lower()  # Convert email to lower case
+        # Convert email to lower case
+        email = request.form.get("email").lower()
         password = request.form.get("password")
         confirm_password = request.form.get("confirm_password")
-        
         # Check if the email already exists
         existing_user = mongo.db.users.find_one({"email": email})
         if existing_user:
-            flash("Email already exists. Please use a different email.", "danger")
+            flash(
+                "Email already exists. Please use a different email.", "danger"
+            )
             return redirect(url_for("signup"))
 
         # Check if passwords match
@@ -79,14 +83,14 @@ def signup():
     # Render the signup template
     return render_template("signup.html")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         # Get form data
-        email = request.form.get("email").lower()  # Convert email to lower case
+        email = request.form.get("email").lower()
         password = request.form.get("password")
         remember = request.form.get("remember")
-        
         # Find the user by email
         existing_user = mongo.db.users.find_one({"email": email})
 
@@ -98,8 +102,10 @@ def login():
 
                 # Set session to be permanent if remember me is checked
                 if remember:
-                    session.permanent = True  # This sets the session to use the permanent lifetime
-                    app.permanent_session_lifetime = timedelta(days=30)  # Set session lifetime to 30 days
+                    # This sets the session to use the permanent lifetime
+                    session.permanent = True
+                    # Set session lifetime to 30 days
+                    app.permanent_session_lifetime = timedelta(days=30)
 
                 return redirect(url_for("profile", username=email))
             else:
@@ -109,7 +115,6 @@ def login():
         else:
             flash("Incorrect Email and/or Password", 'danger')
             return redirect(url_for("login"))
-    
     # Render the login template
     return render_template("login.html")
 
@@ -149,22 +154,31 @@ def about():
 
 @app.route("/profile/<username>")
 def profile(username):
+    """ Show the profile for that user."""
     # Find the user by email
     user = mongo.db.users.find_one({"email": username})
     if user:
         # Get user's medical records
-        medical_records = list(mongo.db.medical_records.find({"patient_id": user["_id"]}))
-        
+        medical_records = list(
+            mongo.db.medical_records.find({"patient_id": user["_id"]})
+        )
         # Get user's appointments
-        appointments = list(mongo.db.appointments.find({"patient_id": user["_id"]}))
-        
+        appointments = list(
+            mongo.db.appointments.find({"patient_id": user["_id"]})
+        )
         # Render the profile template with user data
-        return render_template("profile.html", user=user, medical_records=medical_records, appointments=appointments)
+        return render_template(
+            "profile.html",
+            user=user,
+            medical_records=medical_records,
+            appointments=appointments
+        )
     else:
-        # This case should not occur if login logic is correct, but it's a good safety measure
+        # This case should not occur if login logic is correct,
+        # but it's a good safety measure
         flash("User not found", "danger")
         return redirect(url_for("index"))
-    
+
 
 @app.route("/edit_user_ajax", methods=["POST"])
 def edit_user_ajax():
@@ -176,7 +190,10 @@ def edit_user_ajax():
 
             # Verify current password
             if not check_password_hash(user["password"], current_password):
-                return {"success": False, "message": "Current password is incorrect."}, 403
+                return {
+                    "success": False, "message":
+                    "Current password is incorrect."
+                }, 403
 
             # Get form data
             name = request.json.get("name")
@@ -189,7 +206,8 @@ def edit_user_ajax():
             update_data = {
                 "name": name,
                 "gender": gender,
-                "dob": dob if dob else user.get("dob"),  # Ensure dob is not null
+                # Ensure dob is not null
+                "dob": dob if dob else user.get("dob"),
                 "phone": phone,
                 "email": new_email,
             }
@@ -197,7 +215,8 @@ def edit_user_ajax():
             # Allow role change only for admin users
             if user["role"] == "admin":
                 role = request.json.get("role")
-                update_data["role"] = role if role else user.get("role")  # Ensure role is not null
+                # Ensure role is not null
+                update_data["role"] = role if role else user.get("role")
 
             # Update user information in the database
             mongo.db.users.update_one(
@@ -208,19 +227,32 @@ def edit_user_ajax():
             # Update session email if changed
             if current_email != new_email:
                 session["user"] = new_email
-                flash("Your email has been updated to {}.".format(new_email), "success")
-                return {"success": True, "message": "Your email has been updated.", "redirect": url_for("profile", username=new_email)}
+                flash(
+                    "Your email has been updated to {}.".format(new_email),
+                    "success"
+                )
+                return {
+                    "success": True, "message": "Your email has been updated.",
+                    "redirect": url_for("profile", username=new_email)
+                }
             else:
                 flash("Your information has been updated.", "success")
-                return {"success": True, "message": "Your information has been updated.", "redirect": url_for("profile", username=current_email)}
+                return {
+                    "success": True, "message":
+                    "Your information has been updated.",
+                    "redirect": url_for("profile", username=current_email)
+                }
         else:
             return {"success": False, "message": "User not found."}, 404
     else:
-        return {"success": False, "message": "You need to log in to edit your information."}, 403
-    
+        return {
+            "success": False, "message":
+            "You need to log in to edit your information."
+        }, 403
+
 
 if __name__ == "__main__":
     # Run the Flask application
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+    app.run(
+        host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True
+    )
